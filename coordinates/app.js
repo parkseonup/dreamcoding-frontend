1e7a473b1a11dfadc70e74e0102c275810ae6e73
Seonu <seonup95@gmail.com>
2022-04-03 18:27:56 +0900

    성능 개선: top, left -> translate 사용

--- coordinates/app.js
+++ coordinates/app.js
@@ -3,15 +3,27 @@ const horizontal = document.querySelector('.horizontal');
 const vertical = document.querySelector('.vertical');
 const coordinates = document.querySelector('.coordinates');
 
-window.addEventListener('mousemove', (event) => {
-  const x = event.clientX;
-  const y = event.clientY;
+window.addEventListener('load', () => {
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  target.style.top = `${y}px`;
-  target.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
-  vertical.style.left = `${x}px`;
-  coordinates.style.top = `${y}px`;
-  coordinates.style.left = `${x}px`;
-  coordinates.innerText = `${x}px, ${y}px`;
-});
+  window.addEventListener('mousemove', (event) => {
+    const x = event.clientX;
+    const y = event.clientY;
+
+    //target.style.top = `${y}px`;
+    //target.style.left = `${x}px`;
+    //horizontal.style.top = `${y}px`;
+    //vertical.style.left = `${x}px`;
+    //coordinates.style.top = `${y}px`;
+    //coordinates.style.left = `${x}px`;
+    //coordinates.innerText = `${x}px, ${y}px`;
+
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${y - targetHalfHeight}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    vertical.style.transform = `translateX(${x}px)`;
+    coordinates.style.transform = `translate(${x}px, ${y}px)`;
+    coordinates.innerText = `${x}px, ${y}px`;
+  });
+})
